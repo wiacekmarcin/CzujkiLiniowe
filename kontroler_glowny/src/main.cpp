fff0b5ebb1e925ce3b5dbae00c547203078b9ba3
Marcin Wiącek <>
2022-09-23 07:53:03 +0200

    Wersja z działającym testowo SPI

--- kontroler_glowny/src/main.cpp
+++ kontroler_glowny/src/main.cpp
@@ -2,30 +2,89 @@
 #include <SPI.h>
 #include "pins_arduino.h"
 
+#define PIN_P1 46
+#define PIN_P2 44
+#define PIN_P3 42
+#define PIN_RES 40
+
 void setup (void)
 {
   // Put SCK, MOSI, SS pins into output mode
   // also put SCK, MOSI into LOW state, and SS into HIGH state.
   // Then put SPI hardware into Master mode and turn SPI on
   SPI.begin ();
-  SPI.setClockDivider(SPI_CLOCK_DIV8);
+  //SPI.setClockDivider(SPI_CLOCK_DIV128);
   
+  Serial.begin(9600);
+
+  pinMode(PIN_P1, OUTPUT);
+  pinMode(PIN_P2, OUTPUT);
+  pinMode(PIN_P3, OUTPUT);
+  pinMode(PIN_RES, OUTPUT);
+
+  digitalWrite(PIN_RES, LOW);
+  digitalWrite(PIN_P1, LOW);
+  digitalWrite(PIN_P2, LOW);
+  digitalWrite(PIN_P3, LOW);
+  delay(1000);
+  digitalWrite(PIN_RES, HIGH);
+  delay(1000);
+  digitalWrite(PIN_P1, HIGH);
+  delay(1000);
+  digitalWrite(PIN_P2, HIGH);
+  delay(1000);
+  digitalWrite(PIN_P3, HIGH);
+  delay(1000);
 }  // end of setup
+
+volatile char buf[5];
+volatile byte pos = 0;
 
 void loop (void)
 {
-
-  char c;
-
-  // enable Slave Select
-  digitalWrite(SS, LOW);    // SS is pin 10
-
-  // send test string
-  for (const char * p = "Hello, world!\n" ; c = *p; p++)
-    SPI.transfer (c);
-
-  // disable Slave Select
-  digitalWrite(SS, HIGH);
-
-  delay (1000);  // 1 seconds delay 
-}  // end of loop
+  if (Serial.available() > 0) {
+    int c = Serial.read();
+    if (c > 0) {
+      buf[pos++] = c;
+      if (c == '\n') {
+        if (buf[0] == 'A') {
+          Serial.print("\nTransfer=");
+          Serial.print(buf[1]);
+          Serial.println("Read=");
+          digitalWrite(PIN_P1, LOW); 
+          Serial.println(SPI.transfer (buf[1]), HEX);
+          digitalWrite(PIN_P1, HIGH);
+          pos = 0;
+        } else if (buf[0] == 'B') {
+          Serial.print("\nTransfer=");
+          Serial.print(buf[1]);
+          Serial.println("Read=");
+          digitalWrite(PIN_P2, LOW); 
+          Serial.println(SPI.transfer (buf[1]), HEX);
+          digitalWrite(PIN_P2, HIGH);
+          pos = 0;
+        } else if (buf[0] == 'C') {
+          Serial.print("\nTransfer=");
+          Serial.print(buf[1]);
+          Serial.println("Read=");
+          digitalWrite(PIN_P3, LOW); 
+          Serial.println(SPI.transfer (buf[1]), HEX);
+          digitalWrite(PIN_P3, HIGH);
+          pos = 0;
+        } else if (buf[0] == 'R') {
+          Serial.println("\nReset");
+          digitalWrite(PIN_RES, LOW);
+          delay(100);
+          digitalWrite(PIN_RES, HIGH);
+          pos = 0;
+          return;
+        }
+      } else {
+        if (pos > 5) {
+          pos = 0;
+        }
+        Serial.print((char)c);
+      }
+    }
+  }
+}
--- kontroler_silnikow/src/main.cpp
+++ kontroler_silnikow/src/main.cpp
@@ -1,27 +1,71 @@
 #include <Arduino.h>
 #include "pins_arduino.h"
 
-char buf [100];
-volatile byte pos;
-volatile boolean process_it;
+//SPI SLAVE (ARDUINO)
+#include<SPI.h>
 
+volatile boolean received;
+volatile byte Slavereceived,Slavesend;
+#define LEDPIN 9
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(LEDPIN,OUTPUT);                 // Setting pin 7 as OUTPUT
+  pinMode(MISO,OUTPUT);                   //Sets MISO as OUTPUT (Have to Send data to Master IN 
+
+  SPCR |= _BV(SPE);                       //Turn on SPI in Slave Mode
+  received = false;
+  SPI.attachInterrupt();                  //Interuupt ON is set for SPI commnucation
+
+  digitalWrite(LEDPIN, HIGH);
+  delay(2000);
+  digitalWrite(LEDPIN, LOW);
+}
+
+ISR (SPI_STC_vect)                        //Inerrrput routine function 
+{
+  Slavereceived = SPDR;         // Value received from master if store in variable slavereceived
+  received = true;                        //Sets received as True 
+}
+
+void loop()
+{ 
+  if(received)                            //Logic to SET LED ON OR OFF depending upon the value recerived from master
+  {
+    received = false;
+    Serial.print("Receive");
+    Serial.println(Slavereceived);
+    if (Slavereceived=='1') 
+    {
+      digitalWrite(LEDPIN,HIGH);         //Sets pin 7 as HIGH LED ON
+      Serial.println("Slave LED ON");
+    } else {
+      digitalWrite(LEDPIN,LOW);          //Sets pin 7 as LOW LED OFF
+      Serial.println("Slave LED OFF");
+    }
+    Slavesend=(0xff ^ Slavereceived);                             
+    SPDR = Slavesend;                           //Sends the x value to master via SPDR 
+    Serial.print("Send");
+    Serial.println(Slavesend, HEX);
+  }
+}
+
+/*
 void setup (void)
 {
-  Serial.begin (9600);   // debugging
-
+  pinMode(LEDPIN, OUTPUT);
   // have to send on master in, *slave out*
   pinMode(MISO, OUTPUT);
-  
   // turn on SPI in slave mode
   SPCR |= _BV(SPE);
-  
   // turn on interrupts
   SPCR |= _BV(SPIE);
-  
   pos = 0;
   process_it = false;
+  digitalWrite(LEDPIN, HIGH);
+  delay(2000);
+  digitalWrite(LEDPIN, LOW);
 }  // end of setup
-
 
 // SPI interrupt routine
 ISR (SPI_STC_vect)
@@ -45,12 +89,14 @@ void loop (void)
 {
   if (process_it)
     {
-    buf [pos] = 0;  
-    Serial.println (buf);
-    pos = 0;
-    process_it = false;
-    }  // end of flag set
-    
+      char c = buf[pos-1];
+      pos = 0;
+      process_it = false;
+      if (c == '0')
+        digitalWrite(LEDPIN, LOW);
+      else if (c == '1')
+        digitalWrite(LEDPIN, HIGH);
+    }  // end of flag set 
 }  // end of loop
-
+*/
 
